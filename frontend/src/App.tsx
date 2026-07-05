38c84096c508d5f8d50731366aa53ace8825f287
HARI OM PANDEY <hariompande824@gmail.com>
2026-07-05 16:02:37 +0530

    fix: track clicks for frontend redirect and password links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,9 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-  <Route path="/" element={<Navigate to="/login" />} />
+  <Route path="/" element={localStorage.getItem("token")?(
+    <Navigate to="/dashboard" replace />
+  ) : ( <Navigate to="/login" replace />)} />
   <Route path="/login" element={<Login />} />
   <Route path="/register" element={<Register />} />
 
